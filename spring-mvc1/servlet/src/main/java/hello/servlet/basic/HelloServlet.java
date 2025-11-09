62a18e2c06d239e3b2c0c8a534d9a8839e144dc7
김흥석 <eovnfjfpa@naver.com>
2025-11-09 21:17:33 +0900

    6일차 스프링 MVC1편 섹션3 서블릿 까지

---
+++ spring-mvc1/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello") //hello 가 오면 이게 실행됨
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("res = " + response);
+
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
