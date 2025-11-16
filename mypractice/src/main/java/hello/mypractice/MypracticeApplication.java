e6947340fef1f17957b36918abbcebd06f8de2ad
김흥석 <eovnfjfpa@naver.com>
2025-11-16 14:19:36 +0900

    13일차스프링 MVC 활용 웹페이지 연습 폴더 추가

---
+++ mypractice/src/main/java/hello/mypractice/MypracticeApplication.java
@@ -0,0 +1,13 @@
+package hello.mypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypracticeApplication.class, args);
+	}
+
+}
---
+++ mypractice/src/test/java/hello/mypractice/MypracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.mypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
